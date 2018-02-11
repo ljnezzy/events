6f17a73001a230c1f211560f48569f4bb7505db0
Larry Nesbit <larryjnesbit@gmail.com>
2018-02-11 01:11:11 -0600

    Ready for deployment

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,58 @@
+class RegistrationsController < ApplicationController
+	before_action :set_event
+
+	def index
+    #@event = Event.find(params[:event_id])
+    @registrations = @event.registrations
+    #@registrations = Registration.all
+	end
+
+	def new
+   	@registration = @event.registrations.new
+  end
+
+	def create
+	  @registration = 
+	    @event.registrations.new(registration_params)
+	  if @registration.save
+	    redirect_to event_registrations_url(@event), 
+	                notice: "Thanks for registering!"
+	  else
+	    render :new
+	  end
+	end
+
+  def show
+    @registration = Registration.find(params[:id])
+  end
+
+  def edit
+    @registration = Registration.find(params[:id])
+  end
+
+  def update
+    @registration = Registration.find(params[:id])
+    if @registration.update(registration_params)
+      redirect_to event_registrations_path(@event), notice: "Registration successfully updated!" 
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @registration = Registration.find(params[:id])
+    @registration.destroy
+    redirect_to event_registrations_path(@event)
+  end
+
+
+private
+
+	def registration_params
+	  params.require(:registration).permit(:name, :email, :how_heard)
+	end
+
+	def set_event
+    	@event = Event.find(params[:event_id])
+  end
+end
